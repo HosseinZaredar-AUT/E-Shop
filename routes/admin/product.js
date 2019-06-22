6858fe98f7b43a31e1b15221db2ef560cf071e5f
Hossein Zaredar <hossein.zare9978@gmail.com>
2019-06-22 14:05:47 +0430

    Changes in saving uploaded images

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -2,6 +2,7 @@ let express = require('express'),
     router = express.Router(),
     path = require('path'),
     fs = require('fs'),
+    rimraf = require('rimraf'),
     filepond = require('filepond'),
     uuid = require('uuid/v1'),
     Category = require('../../moduls/post/category'),
@@ -14,26 +15,44 @@ router.get('/', function (req,res) {
 });
 
 router.post('/', function (req, res) {
-    let properties = req.body.property;
-    let propertiesArray = [];
-    for(let i=0;i<properties.length-1;i++) {
-        propertiesArray.push(new Property({
-            key: properties[i],
-            value: properties[++i]
-        }));
-    }
-    Category.findOne({name: req.body.category}, function (err, foundCategory) {
-        if(!err) {
+    // let properties = req.body.property;
+    // let propertiesArray = [];
+    // for(let i=0;i<properties.length-1;i++) {
+    //     propertiesArray.push(new Property({
+    //         key: properties[i],
+    //         value: properties[++i]
+    //     }));
+    // }
+    // Category.findOne({name: req.body.category}, function (err, foundCategory) {
+    //     if(!err) {
             let product = new Product({
-                name: req.body.name,
-                description: req.body.description,
-                remainingNumber: req.body.remainingNumber,
-                price: req.body.price,
-                hasDiscount: req.body.hasDiscount,
-                category: foundCategory._id,
-                properties: propertiesArray,
+                // name: req.body.name,
+                // description: req.body.description,
+                // remainingNumber: req.body.remainingNumber,
+                // price: req.body.price,
+                // hasDiscount: req.body.hasDiscount,
+                // category: foundCategory._id,
+                // properties: propertiesArray,
+                images: []
             });
-            // TODO file still need to be handled
+
+            // saving images and removing them from temp_images
+            fs.mkdirSync('public/Images/products/' + product._id);
+
+            // if it is a single images
+            if (typeof(req.body.filepond) == 'string') {
+                image = req.body.filepond;
+                fs.renameSync('./temp_images/' + image, './public/Images/products/' + product._id + '/' + image);
+                product.images.push('Images/products/' + product._id + '/' + image);
+                rimraf.sync('./temp_images/');
+            } else { // if it is an array of images
+                for (image of req.body.filepond) {
+                    fs.renameSync('./temp_images/' + image, './public/Images/products/' + product._id + '/' + image);
+                    product.images.push('Images/products/' + product._id + '/' + image);
+                }
+                rimraf.sync('./temp_images/');
+            }
+            
             product.save(function (err, savedProduct) {
                 if(!err) {
                     console.log(savedProduct);
@@ -42,8 +61,8 @@ router.post('/', function (req, res) {
                     console.log(err);
                 }
             })
-        }
-    })
+        // }
+    // })
 });
 
 router.put('/:id', function (req, res) {
@@ -78,35 +97,42 @@ router.put('/:id', function (req, res) {
     })
 });
 
-router.delete('/:id', function (req, res) {
-    Product.findOneAndDelete({id: req.params.id}, function (err) {
-        if(!err) {
-            // TODO product photos must be deleted
-            res.redirect('/');
-        }
-    })
-});
-
 // to upload a file as it is dragged into filepond
 router.post('/upload', function(req, res) {
     var name = uuid();
     var fullType = req.files.filepond.mimetype;
     var type = fullType.substring(fullType.indexOf('/') + 1);
-    req.files.filepond.mv('./public/images/' + name + '.' + type).then(function(err) {
+
+    // creating temp_images directory if it doesn't exist
+    var dir = './temp_images';
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+
+    // saving the image in temp_images
+    req.files.filepond.mv('./temp_images/' + name + '.' + type).then(function(err) {
         if (err)
             throw err;
-        var path = 'images/' + name + '.' + type;
-        res.send(path);
+        res.send(name + '.' + type);
     });
 });
 
 // to revert file upload
-// should use a delete method but it didn't work, I don't know why
-router.post('/revert', function(req, res) {
-    fs.unlink('./public/' + req.body.path, function(err) {
+router.delete('/revert', function(req, res) {
+    fs.unlink('./temp_images/' + req.body.path, function(err) {
         if (err)
             console.log(err);
     });
+});
+
+router.delete('/:id', function (req, res) {
+    Product.findOneAndDelete({id: req.params.id}, function (err) {
+        if(!err) {
+            rimraf('./public/Images/products/' + id, function() {
+                res.redirect('/');
+            });
+        }
+    })
 });
 
 
