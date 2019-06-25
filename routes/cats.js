b480d084ca8287529c1435dc427aacac3ecd61c4
Shayan Shafaghi <sh.shafaghi@aut.ac.ir>
2019-06-25 21:38:50 +0430

    renderingSpecificCat

--- routes/cats.js
+++ routes/cats.js
@@ -4,4 +4,6 @@ const catsController = require('../Controllers/CatsController');
 
 router.get('/', catsController.getProducts);
 
+router.get('/:catName', catsController.getCatProduct);
+
 module.exports = router;
