1948ba439293eb9240d3a298aab610e1d4586d26
Shayan Shafaghi <sh.shafaghi@aut.ac.ir>
2019-06-25 15:19:58 +0430

    catPageDynamic
    Dynamic prods and dynamic pagination

--- Controllers/CatsController.js
+++ Controllers/CatsController.js
@@ -1,18 +1,35 @@
 Product = require('../moduls/post/product');
 
+let ITEMS_PER_PAGE = 1;
+
 exports.getProducts = async (req, res, next) => {
     try {
-        const products = await Product.find();
-        console.log('received:');
-        console.log(products);
-        // res.render('', {
-        //     prods: products,
-        //     pageTitle: 'محصولات'
-        // });
-        res.render('product/category')
+        let page = req.query.page;
+        if (page === undefined) {
+            page = 1;
+        }
+        console.log(page);
+        const count = await Product.find().countDocuments();
+        console.log(count);
+        const products = await Product.find().skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+        const lastPage = Math.floor((count - 0.5) / ITEMS_PER_PAGE + 1);
+        console.log("lastPage: " + lastPage);
+        const start = (page - 1) * ITEMS_PER_PAGE + 1;
+        console.log("here");
+        const end = start + products.length - 1;
+        res.render('product/category', {
+            prods: products,
+            pageTitle: 'محصولات',
+            page: page,
+            lastPage: lastPage,
+            paths: [],
+            start: start,
+            end: end
+        });
     } catch (err) {
         // const error = new Error(err);
         // error.httpStatusCode = 500;
         // return next(error);
     }
+
 };
