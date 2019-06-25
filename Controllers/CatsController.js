35b64f21ce35d73273261ca52b585320f7fbb212
Shayan Shafaghi <sh.shafaghi@aut.ac.ir>
2019-06-26 00:47:05 +0430

    sortAdded

--- Controllers/CatsController.js
+++ Controllers/CatsController.js
@@ -5,14 +5,28 @@ let ITEMS_PER_PAGE = 1;
 
 exports.getProducts = async (req, res, next) => {
     try {
+        let products = null;
         let page = req.query.page;
+        let sort = req.query.sort;
+        console.log(sort);
         if (page === undefined) {
             page = 1;
+        }
+        if (sort === undefined) {
+            sort = 'new';
         }
         console.log(page);
         const count = await Product.find().countDocuments();
         console.log(count);
-        const products = await Product.find().skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+        if (sort === 'new') {
+            products = await Product.find().sort('date').skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+        } else if (sort === 'popular') {
+            products = await Product.find().sort({boughtNumber: -1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+        } else if (sort === 'cheap') {
+            products = await Product.find().sort({price: 1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+        } else if (sort === 'exp') {
+            products = await Product.find().sort({price: -1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+        }
         const lastPage = Math.floor((count - 0.5) / ITEMS_PER_PAGE + 1);
         console.log("lastPage: " + lastPage);
         const start = (page - 1) * ITEMS_PER_PAGE + 1;
@@ -22,6 +36,8 @@ exports.getProducts = async (req, res, next) => {
         res.render('product/category', {
             prods: products,
             pageTitle: 'محصولات',
+            dir: '',
+            sort: sort,
             page: page,
             lastPage: lastPage,
             paths: [],
@@ -39,11 +55,16 @@ exports.getCatProduct = async (req, res, next) => {
     try {
         let page = req.query.page;
         let catName = req.params.catName;
+        let sort = req.query.sort;
+        console.log('ssr' + sort);
         if (page === undefined) {
             page = 1;
         }
-        // console.log(catName);
-        // console.log(page);
+        if (sort === undefined) {
+            sort = 'new';
+        }
+        console.log('ssrfgg' + sort);
+
         const popCat = await Category.find({name: catName}).populate('products');
         allProducts = popCat[0].products;
         console.log(allProducts[0]);
@@ -56,7 +77,9 @@ exports.getCatProduct = async (req, res, next) => {
         console.log(products.colors);
         res.render('product/category', {
             prods: products,
-            pageTitle: 'محصولات',
+            pageTitle: catName,
+            dir: catName,
+            sort: sort,
             page: page,
             lastPage: lastPage,
             paths: [],
