28f253e571daf44c39b2e207667048bd2a09d9bf
Shayan Shafaghi <sh.shafaghi@aut.ac.ir>
2019-06-27 03:40:22 +0430

    CatPageCompleted

--- Controllers/CatsController.js
+++ Controllers/CatsController.js
@@ -8,41 +8,57 @@ exports.getProducts = async (req, res, next) => {
         let products = null;
         let page = req.query.page;
         let sort = req.query.sort;
-        console.log(sort);
+        let colors = req.query.color;
+        let brands = req.query.brand;
+        let price = req.query.price;
+        let colorsFiltered = true;
+        let brandsFiltered = true;
         if (page === undefined) {
             page = 1;
         }
         if (sort === undefined) {
             sort = 'new';
         }
-        console.log(page);
-        const count = await Product.find().countDocuments();
-        console.log(count);
+        if (colors === undefined) {
+            colorsFiltered = false;
+            colors = ["#000000", "#ff0000", "#0080ff", "#00ff00"];
+        }
+        if (brands === undefined) {
+            brandsFiltered = false;
+            brands = ["samsung", "LG", "Apple", "HTC"];
+        }
+        if (price === undefined) {
+            price = 100000;
+        }
+        const count = await Product.find().where('colors').in(colors).where('price').lte(price).where('properties.value').in(brands).countDocuments();
         if (sort === 'new') {
-            products = await Product.find().sort('date').skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+            products = await Product.find().where('colors').in(colors).where('price').lte(price).where('properties.value').in(brands).sort({creationDate: -1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
         } else if (sort === 'popular') {
-            products = await Product.find().sort({boughtNumber: -1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+            products = await Product.find().where('colors').in(colors).where('price').lte(price).where('properties.value').in(brands).sort({boughtNumber: -1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
         } else if (sort === 'cheap') {
-            products = await Product.find().sort({price: 1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+            products = await Product.find().where('colors').in(colors).where('price').lte(price).where('properties.value').in(brands).sort({price: 1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
         } else if (sort === 'exp') {
-            products = await Product.find().sort({price: -1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+            products = await Product.find().where('colors').in(colors).where('price').lte(price).where('properties.value').in(brands).sort({price: -1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
         }
         const lastPage = Math.floor((count - 0.5) / ITEMS_PER_PAGE + 1);
-        console.log("lastPage: " + lastPage);
         const start = (page - 1) * ITEMS_PER_PAGE + 1;
-        console.log("here");
         const end = start + products.length - 1;
-        console.log(products);
         res.render('product/category', {
             prods: products,
-            pageTitle: 'محصولات',
+            count: count,
+            colors:colors,
+            price:price,
+            brands: brands,
+            pageTitle: 'مصولات',
             dir: '',
             sort: sort,
             page: page,
             lastPage: lastPage,
             paths: [],
             start: start,
-            end: end
+            end: end,
+            colorsFiltered: colorsFiltered,
+            brandsFiltered: brandsFiltered
         });
     } catch (err) {
         // const error = new Error(err);
@@ -56,27 +72,84 @@ exports.getCatProduct = async (req, res, next) => {
         let page = req.query.page;
         let catName = req.params.catName;
         let sort = req.query.sort;
-        console.log('ssr' + sort);
+        let colors = req.query.color;
+        let brands = req.query.brand;
+        let price = req.query.price;
+        let colorsFiltered = true;
+        let brandsFiltered = true;
         if (page === undefined) {
             page = 1;
         }
         if (sort === undefined) {
             sort = 'new';
         }
-        console.log('ssrfgg' + sort);
-
-        const popCat = await Category.find({name: catName}).populate('products');
+        if (colors === undefined) {
+            colorsFiltered = false;
+            colors = ["#000000", "#ff0000", "#0080ff", "#00ff00"];
+        }
+        if (brands === undefined) {
+            brandsFiltered = false;
+            brands = ["samsung", "LG", "Apple", "HTC"];
+        }
+        if (price === undefined) {
+            price = 100000;
+        }
+        console.log(brands);
+        const popCat = await Category.find({name: catName}).populate({
+            path: 'products',
+            match: {
+                price: {$lte: price},
+                colors: {$in: colors},
+                'properties.value': {$in: brands}
+            }
+        });
         allProducts = popCat[0].products;
-        console.log(allProducts[0]);
+        console.log(allProducts);
         const count = allProducts.length;
         let begin = (page - 1) * ITEMS_PER_PAGE;
-        const products = allProducts.slice(begin, begin + ITEMS_PER_PAGE);
+        if (sort === 'new') {
+            allProducts.sort((a, b) => {
+                if (a.creationDate < b.creationDate) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+        } else if (sort === 'popular') {
+            allProducts.sort((a, b) => {
+                if (a.boughtNumber < b.boughtNumber) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+        } else if (sort === 'cheap') {
+            allProducts.sort((a, b) => {
+                if (a.price > b.price) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+        } else if (sort === 'exp') {
+            allProducts.sort((a, b) => {
+                if (a.price < b.price) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+        }
+        let products = allProducts.slice(begin, begin + ITEMS_PER_PAGE);
         const lastPage = Math.floor((count - 0.5) / ITEMS_PER_PAGE + 1);
         const start = (page - 1) * ITEMS_PER_PAGE + 1;
         const end = start + products.length - 1;
-        console.log(products.colors);
         res.render('product/category', {
             prods: products,
+            count: count,
+            colors:colors,
+            price:price,
+            brands: brands,
             pageTitle: catName,
             dir: catName,
             sort: sort,
@@ -84,7 +157,9 @@ exports.getCatProduct = async (req, res, next) => {
             lastPage: lastPage,
             paths: [],
             start: start,
-            end: end
+            end: end,
+            colorsFiltered: colorsFiltered,
+            brandsFiltered: brandsFiltered
         });
 
     } catch (err) {
