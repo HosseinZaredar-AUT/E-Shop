ea9df93b57a25b7b3340778fec9b6a88cc471bcc
Hossein Zaredar <hossein.zare9978@gmail.com>
2019-06-26 11:31:24 +0430

    Fixed admin login

--- passport.js
+++ passport.js
@@ -1,22 +1,36 @@
 var LocalStrategy = require('passport-local').Strategy,
     mongoose = require('mongoose'),
     bcrypt = require('bcrypt'),
-    Customer = require('./moduls/user/customer');
+    Customer = require('./moduls/user/customer'),
+    Admin = require('./moduls/user/admin');
 
 module.exports = function(passport) {
     passport.use(
       new LocalStrategy({usernameField: 'email', passwordField: 'password'},
         function(email, password, done) {
-            Customer.findOne({email: email}, function(err, customer) {
-                if (!customer)
-                    return done(null, false);
-                
-                bcrypt.compare(password, customer.password, function(err, isEqual) {
-                    if (isEqual)
-                        return done(null, customer);
-                    else
-                        return done(null, false);
-                });
+            // checking if the user is admin
+            Admin.findOne({email: email}, function(err, admin) {
+                if (admin) {
+                    bcrypt.compare(password, admin.password, function(err, isEqual) {
+                        if (isEqual)
+                            return done(null, admin);
+                        else
+                            return done(null, false);
+                    });                 
+                } else {
+                    // chceking if the user is a customer
+                    Customer.findOne({email: email}, function(err, customer) {
+                        if (!customer)
+                            return done(null, false);
+                        
+                        bcrypt.compare(password, customer.password, function(err, isEqual) {
+                            if (isEqual)
+                                return done(null, customer);
+                            else
+                                return done(null, false);
+                        });
+                    });
+                }
             });
         })
     );
